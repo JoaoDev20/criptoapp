4db94b3157246e0392fae7ac1800b5668c20d861
JoaoDev20 <joaovictorapso06@gmail.com>
2026-01-24 10:48:05 -0300

    Estilizando a lista da pagina home, dexando responsiva para aparelho mobile

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -49,7 +49,7 @@ export function Home() {
                 2B
               </td>
 
-              <td className={styles.tdLabel} data-label="Mudança 24H">
+              <td className={styles.tdProfit} data-label="Mudança 24H">
                 <span>1.20</span>
               </td>
             </tr>
