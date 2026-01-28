29be0548bec8467f7eb30ac949900d60a0f4757a
JoaoDev20 <joaovictorapso06@gmail.com>
2026-01-28 15:43:43 -0300

    Criando págia detalhes dasmoeda individualmente

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,8 +1,71 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { type CoinProps } from "../home";
+
+interface ResponseData {
+  data: CoinProps;
+}
+
+interface ErroData {
+  error: string;
+}
+
+type DataProp = ResponseData | ErroData;
+
 export function Detail() {
+  const { cripto } = useParams();
+  const navigate = useNavigate();
+  const [coin, setCoin] = useState<CoinProps>();
+
+  useEffect(() => {
+    async function getCoin() {
+      try {
+        fetch(
+          `https://rest.coincap.io/v3/assets/${cripto}?apiKey=cd44abd9414edd577d8bc9c39d391464e40c533118cedeed86b44cecb6b34834`,
+        )
+          .then((Response) => Response.json())
+          .then((data: DataProp) => {
+            if ("error" in data) {
+              navigate("/");
+              return;
+            }
+
+            const price = Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            });
+
+            const priceCompact = Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+              notation: "compact",
+            });
+
+            const resultData = {
+              ...data.data,
+              formatedPrice: price.format(Number(data.data.priceUsd)),
+              formatedMarket: priceCompact.format(
+                Number(data.data.marketCapUsd),
+              ),
+              formatedVolume: priceCompact.format(
+                Number(data.data.volumeUsd24Hr),
+              ),
+            };
+
+            setCoin(resultData);
+          });
+      } catch (erro) {
+        console.log(erro);
+        navigate("/");
+      }
+    }
+
+    getCoin();
+  }, [cripto]);
   return (
     <>
       <div>
-        <h1>Página Detalhe da moeda</h1>
+        <h1>Página Detalhe da moeda {cripto}</h1>
       </div>
     </>
   );
